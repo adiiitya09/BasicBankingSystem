fde2f44cf13e97b9b2ad9ccc1878b2a8d4ecc2fc
aditya <adityasungar99@gmail.com>
2022-01-11 17:40:52 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+
+    <title>Banking System</title>
+  </head>
+
+  <body style="background-color: black;">
+  <header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('img/adi.jpg');
+    /* outline: 2px solid black; */
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    width: 100%;
+    /* background-position: 1px 10px 5px 10px; */
+    
+    
+  }
+</style>
+ <h1 style = "position:relative; left:654px; top:187px; color: #ffe52c ;font-family: 'Permanent Marker', cursive;font-size: 61px;">WELCOME </h1>
+ <h1 style = "position:relative; left:723px; top:177px; color: #ffe52c;  font-family:'Permanent Marker', cursive ;font-size: 61px;">To</h1>
+<h1 style = "position:relative; left:665px; top:165px; color: #ffe52c;  font-family: 'Permanent Marker', cursive;font-size: 61px;">CENTRAL</h1>
+<h1 style = "position:relative; left:689px; top:153px; color: #ffe52c;  font-family: 'Permanent Marker', cursive;font-size: 61px;">BANK</h1>
+<!-- <h1 style = "position:relative; left:85px; top:80px; color:yellow;  font-family: Comic Sans MS;">O</h1>
+<h1 style = "position:relative; left:80px; top:80px; color:yellow;  font-family: Comic Sans MS;">M</h1>
+<h1 style = "position:relative; left:88px; top:80px; color:yellow;  font-family: Comic Sans MS;">E</h1>  --> 
+    </main>
+  </body>
+</html>
